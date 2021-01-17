a71f21aaaa128df302952c8cce39c784192e0e60
sion <sion.andreu@gmail.com>
2021-01-17 19:07:53 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/grup/NotibmsGrupServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.grup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsGrupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsGrupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/grup/NotibmsGrupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.grup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsGrupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
